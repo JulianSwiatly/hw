30728d7e83582539dad4a2602ff6702d114dbe32
JulianSwiatly <62251523+JulianSwiatly@users.noreply.github.com>
2020-03-23 15:15:29 +0100

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -49,7 +49,20 @@ def top5_countries_by_date(day: int, month: int, year: int = 2020) -> List[str]:
     """
 
     # Your code goes here (remove pass)
-    pass
+    def top5_countries_by_date(day: int, month: int, year: int = 2020) -> List[str]:
+      data = f"{month}/{day}/{year - 2000}"
+      grouped_cases=confirmed_cases.groupby(["Country/Region"]).max()
+      sorted_cases=grouped_cases.sort_values(by=data, ascending=False).head(5)
+      a=sorted_cases.index.values.tolist()
+      return a
+ CONFIRMED_CASES_URL = f"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
+    confirmed_cases = pd.read_csv(CONFIRMED_CASES_URL, error_bad_lines=False)
+    date = datetime.date(year, month, day)
+    date1 = datetime.date(year, month, day).strftime('%-m/%-d/%-y')
+    date2 = datetime.date(year, month, day) + datetime.timedelta(days=-1)
+    date2 = date2.strftime('%-m/%-d/%-y')
+    return len(confirmed_cases.loc[confirmed_cases[date2] != confirmed_cases[date1]])
+
 
 
 def no_new_cases_count(day: int, month: int, year: int = 2020) -> int:
@@ -69,4 +82,11 @@ def no_new_cases_count(day: int, month: int, year: int = 2020) -> int:
     """
     
     # Your code goes here (remove pass)
-    pass
+ import datetime
+
+  def no_new_cases_count(day: int, month: int, year: int = 2020) -> int:
+      date = datetime.date(year, month, day)
+      yesterday = datetime.date(year, month, day)+datetime.timedelta(days=-1)
+      d = date.strftime('%-m/%-d/%y")
+      new_caes = confirmed_cases.loc[(confirmed_cases[d]!=confirmed_cases[y])].count()[1]
+      return int(new_cases)
